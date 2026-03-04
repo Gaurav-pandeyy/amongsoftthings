55a51bcf710119ba71e6fc1315d1f363b9ca80d3
Gaurav-pandeyy <gauravpande568@gmail.com>
2026-03-04 11:49:32 +0530

    removed the made with rocket thing

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,8 +26,7 @@ export default function RootLayout({
         <html lang="en">
             <body>{children}
 
-                <script type="module" async src="https://static.rocket.new/rocket-web.js?_cfg=https%3A%2F%2Famongsoftt8069back.builtwithrocket.new&_be=https%3A%2F%2Fappanalytics.rocket.new&_v=0.1.17" />
-                <script type="module" defer src="https://static.rocket.new/rocket-shot.js?v=0.0.2" /></body>
+            </body>
         </html>
     );
 }
